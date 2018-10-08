c8b7c2bc96de3e0657f3146c9b25cc2f3e4d4c00
mdoubah87 <mdoubah1410@gmail.com>
2018-10-08 16:25:18 +0000

    Ajout des fichiers extensions cheveux

--- CATEGORIES/meche_et_extension_de_cheveux/data/meche_et_extension_de_cheveux.js
+++ CATEGORIES/meche_et_extension_de_cheveux/data/meche_et_extension_de_cheveux.js
@@ -2,31 +2,204 @@
     {
         key: 'MeggVGhhg3144',
         imgUrl: require(''),
-        name: '',
+        name: 'Meches et Cheveux',
         subCategories: [
             {
-                key: '',
-                imgUrl: require(''),
-                name: 'Vêtements de Sports',
+                key: 'MeggVGhhKKi88',
+                imgUrl: require('../Images/Cheveux Tisse/P1/C11.png'),
+                name: 'Cheveux Tissages',
                     products: [
                         {
-                            key: 'MeggVGhhg6221',
+                            key: 'MeggVGhhg62G1',
                             imgUrl: require(''),
-                            name: "",
+                            name: "Daimer 3 Tissage Bresilien",
                             description: {
                                 features: [{
-                                    info: "",
+                                    info: "Daimer 3 Tissage Bresilien Ondulé Body Wave en lot Cheveux Humains Brésilien en lot pas cher Naturel 8 10 12",
                                 },]
                                 
                             },
                             productsImg: [
-                                require(''),
-                                require(''),
-                                require(''),
-                                require(''),
+                                require('../Images/Cheveux Tisse/P1/C12.png'),
+                                require('../Images/Cheveux Tisse/P1/C13.png'),
+                                require('../Images/Cheveux Tisse/P1/C14.png'),
+                                require('../Images/Cheveux Tisse/P1/C15.png'),
+                                require('../Images/Cheveux Tisse/P1/C16.png'),
                             ],
-                            views: "",
-                            wishlist: "",
+                            views: "2330",
+                            wishlist: "5106",
+                        },
+                        {
+                            key: 'MeggVGhhg762v6',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C21.png'),
+                            name: "Razeal ",
+                            description: {
+                                features: [{
+                                    info: "Razeal Lot de 5 extensions de cheveux naturels tressés et crépus, tissées au crochet, style Afro - Noir",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C22.png'),
+                                require('../Images/Cheveux Tisse/P1/C23.png'),
+                                require('../Images/Cheveux Tisse/P1/C24.png'),
+                                require('../Images/Cheveux Tisse/P1/C25.png'),
+                            ],
+                            views: "9008",
+                            wishlist: "1008",
+                        },
+                        {
+                            key: 'MeggQdhhg614',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C31.png'),
+                            name: "Hollihi 20 pièces ",
+                            description: {
+                                features: [{
+                                    info: "Grande taille Cheveux Ties Poney Queue de cheval Supports pour cheveux épais – Extensible élastiques Elastiques Boutique tissé Cordes pour les femmes et les filles, Noir",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C32.png'),
+                                require('../Images/Cheveux Tisse/P1/C33.png'),
+                                require('../Images/Cheveux Tisse/P1/C34.png'),
+                                require('../Images/Cheveux Tisse/P1/C35.png'),
+                            ],
+                            views: "8794",
+                            wishlist: "7612",
+                        },
+                        {
+                            key: 'MegDc6hhg6220',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C41.png'),
+                            name: "Extensions de cheveux",
+                            description: {
+                                features: [{
+                                    info: "Extensions de cheveux vierges raides avec fermeture à dentelle, cheveux humains Remy 2 tons à trame tissée de qualité 8A, #T4/30",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C42.png'),
+                                require('../Images/Cheveux Tisse/P1/C43.png'),
+                                require('../Images/Cheveux Tisse/P1/C44.png'),
+                                require('../Images/Cheveux Tisse/P1/C45.png'),
+                            ],
+                            views: "6710",
+                            wishlist: "187",
+                        },
+                        {
+                            key: 'MeggBgGt76',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C51.png'),
+                            name: "Westeng Grand Pince à Cheveux Clips ",
+                            description: {
+                                features: [{
+                                    info: "Westeng Grand Pince à Cheveux Clips Barrettes en Tissu tissé pour Femme Fille 5cm - 1 Pièce - Noir",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C52.png'),
+                                require('../Images/Cheveux Tisse/P1/C53.png'),
+                                require('../Images/Cheveux Tisse/P1/C54.png'),
+                                require('../Images/Cheveux Tisse/P1/C55.png'),
+                            ],
+                            views: "81716",
+                            wishlist: "8871",
+                        },
+                        {
+                            key: 'MegFgTR65jI9',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C61.png'),
+                            name: "Y & M Nouveaux Brésiliens 100%",
+                            description: {
+                                features: [{
+                                    info: "Y & M Nouveaux Brésiliens 100% en de clip extensions des cheveux humains Afro Crespo rICCE ins Clip en cheveux Tisse Naturel Noir 8 pc/",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C61.png'),
+                                require('../Images/Cheveux Tisse/P1/C62.png'),
+                                require('../Images/Cheveux Tisse/P1/C63.png'),
+                                require('../Images/Cheveux Tisse/P1/C64.png'),
+                            ],
+                            views: "7611",
+                            wishlist: "1100",
+                        },
+                        {
+                            key: 'MeggVYyUI891',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C71.png'),
+                            name: "Oylove 9A Grade Mongolian Afro",
+                            description: {
+                                features: [{
+                                    info: "Oylove 9A Grade Mongolian Afro Kinky Curly Clip In Virgin Remy Human Hair 8 pcs 18clips ensemble Vierge Mongole Clip de Cheveux Humains Ins Naturel Noir Couleur 1 faisceau 20 pouces, noir",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C72.png'),
+                                require('../Images/Cheveux Tisse/P1/C73.png'),
+                                require('../Images/Cheveux Tisse/P1/C74.png'),
+                                require('../Images/Cheveux Tisse/P1/C75.png'),
+                            ],
+                            views: "7600",
+                            wishlist: "881",
+                        },
+                        {
+                            key: 'MegDPoI9821',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C81.png'),
+                            name: "4 Paquets d’Extensions Tissées ",
+                            description: {
+                                features: [{
+                                    info: "4 Paquets d’Extensions Tissées des Cheveux Ondulés Brésiliens Cheveux Brésiliens Grade 8a 8 10 12 14 inch",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C82.png'),
+                                require('../Images/Cheveux Tisse/P1/C83.png'),
+                                require('../Images/Cheveux Tisse/P1/C84.png'),
+                                require('../Images/Cheveux Tisse/P1/C85.png'),
+                            ],
+                            views: "10928",
+                            wishlist: "520",
+                        },
+                        {
+                            key: 'MeJUsT624',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/C91.png'),
+                            name: "Les cheveux frisés",
+                            description: {
+                                features: [{
+                                    info: "les cheveux frisés 4 faisceaux avec l'homme libre dans la dentelle fermeture courte tresses kinky curly avec fermeture de faisceaux brésilien des extensions de cheveux 8 cm vierge de 50g paquet couleur naturelle",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/C92.png'),
+                                require('../Images/Cheveux Tisse/P1/C93.png'),
+                                require('../Images/Cheveux Tisse/P1/C94.png'),
+                                require('../Images/Cheveux Tisse/P1/C95.png'),
+                                require('../Images/Cheveux Tisse/P1/C96.png'),
+                            ],
+                            views: "4254",
+                            wishlist: "892",
+                        },
+                        {
+                            key: 'MeKlOAR554',
+                            imgUrl: require('../Images/Cheveux Tisse/P1/101.png'),
+                            name: "Fouriding Mignons",
+                            description: {
+                                features: [{
+                                    info: "Fouriding Mignons Extensions de Cheveux Tissé Chignons Bun Updo Postiche Chouchou Queue de Cheval Femme Mariee Hepburn Volumineux",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../Images/Cheveux Tisse/P1/102.png'),
+                                require('../Images/Cheveux Tisse/P1/103.png'),
+                                require('../Images/Cheveux Tisse/P1/104.png'),
+                                require('../Images/Cheveux Tisse/P1/105.png'),
+                            ],
+                            views: "4237",
+                            wishlist: "876",
                         },
                     ]
             },
