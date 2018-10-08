b41be3aa24edbf6d0f5d2d196e2a7b56e4045d0d
mdoubah87 <mdoubah1410@gmail.com>
2018-10-08 13:13:33 +0000

    Ajout de lextension meche

---
+++ CATEGORIES/meche_et_extension_de_cheveux/data/meche_et_extension_de_cheveux.js
@@ -0,0 +1,35 @@
+[
+    {
+        key: 'MeggVGhhg3144',
+        imgUrl: require(''),
+        name: '',
+        subCategories: [
+            {
+                key: '',
+                imgUrl: require(''),
+                name: 'Vêtements de Sports',
+                    products: [
+                        {
+                            key: 'MeggVGhhg6221',
+                            imgUrl: require(''),
+                            name: "",
+                            description: {
+                                features: [{
+                                    info: "",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require(''),
+                                require(''),
+                                require(''),
+                                require(''),
+                            ],
+                            views: "",
+                            wishlist: "",
+                        },
+                    ]
+            },
+        ]
+    },
+]
