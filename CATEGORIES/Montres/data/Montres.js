2fbfb3de0351a76c6026eaff9e0fbffe910cb1c5
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-09 09:56:46 +0000

    ajout de la structure de l'object dans les categories et les bundles

---
+++ CATEGORIES/Montres/data/Montres.js
@@ -0,0 +1,137 @@
+[
+
+    {
+        key: 'fgkkf84jkdfv57ffifi4jur857',
+        imgUrl: require('../images/1.png'),
+        name: '',
+        subCategories: 
+        [
+            
+            {
+                key: '12ejh5445fghffdfgefe120gg2d3r',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '12ejh45fghffdfgefe12yjyt0gg2d3r',
+                imgUrl: require('../images/pictures//1.png'),
+                name: ' ',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'yyyjnvh67676fghjhhhjhjh',
+                imgUrl: require('../images/pictures//1.png'),
+                name: ' ',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578ts5e3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu65784ts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578rrts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6578tts5rrre3',
+                imgUrl: require('../images/pictures//1.png'),
+                name: '',
+                produits: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            
+            
+        ],
+    },
+
+ 
+]
---
+++ bundles_types/sport.js
@@ -0,0 +1,19 @@
+[
+    {
+        key: '',
+        name: '',
+        bundleImg: '',
+        coverImg:[
+            {
+                img: '',
+                key: '',
+            }
+        ],
+        relatedCategories: [ '', ],
+        sponsoreShops: [{}],
+        starProducts: [{}],
+        newProducts: [{}],
+                
+
+    }
+]
