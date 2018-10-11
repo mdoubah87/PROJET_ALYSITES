62cf7ec2fd221f780de7686ca797e9f52353e07c
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-11 13:07:13 +0000

    fusionner avec la categorie telephone

--- test/categories/data/data.js
+++ test/categories/data/data.js
@@ -1547,4 +1547,356 @@
             
         ],
     }, 
+    {
+        key: 'jvcxf72zx1398jhf76',
+        imgUrl: require('../images/phone.png'),
+        name: 'Telephones portables et Accessoires',
+        subCategories: [
+            {
+                key: 'jvcxf56zx1371jhf43',
+                imgUrl: require('../images/PowerBank/pw4.jpg'),
+                name: 'Power Bank',
+                    products: [
+                        {
+                            key: 'gh5678vbg7',
+                            imgUrl: require('../images/PowerBank/pw5.jpg'),
+                            name: "Battery Charger Solar",
+                            description: {
+                                features: [{
+                                    info: "Battery charger Solar charger Mobile Phones Solar energy - power bank",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/PowerBank/P1/pw1.jpg'),
+                                require('../images/PowerBank/P1/pw2.jpg'),
+                                require('../images/PowerBank/P1/pw3.jpg'),
+                                require('../images/PowerBank/P1/pw4.jpg'),
+                            ],
+                            views: "2453",
+                            wishlist: "456",
+                        },
+                        {
+                            key: 'gf8718vbs4',
+                            imgUrl: require('../images/PowerBank/P2/p21.jpg'),
+                            name: "ADATA PT100 Power Bank",
+                            description: {
+                                features: [{
+                                    info: "ADATA PT100 Power Bank 10,000mAh Turquoise-Pink - Power Bank",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/PowerBank/P2/p22.jgp'),
+                                require('../images/PowerBank/P2/p23.jgp'),
+                                require('../images/PowerBank/P2/p24.jgp'),
+                            ],
+                            views: "2453",
+                            wishlist: "456",
+                        },
+                        {
+                            key: 'gf8718vbs4',
+                            imgUrl: require('../images/PowerBank/P3/p31.jpg'),
+                            name: "Xiaomi Mi",
+                            description: {
+                                features: [{
+                                    info: "Xiaomi Mi Power Bank 10000mAh 2 Quick Charge",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/PowerBank/P3/p32.jpg'),
+                                require('../images/PowerBank/P3/p33.jpg'),
+                                require('../images/PowerBank/P3/p34.jpg'),
+                            ],
+                            views: "3214",
+                            wishlist: "239",
+                        },
+                        {
+                            key: 'gf8718vbs4',
+                            imgUrl: require('../images/PowerBank/P4/p41.jpg'),
+                            name: "Digipower A26",
+                            description: {
+                                features: [{
+                                    info: "Digipower - Power Bank - Black - Larger Front Refuel",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/PowerBank/P4/p42.jpg'),
+                                require('../images/PowerBank/P4/p43.jpg'),
+                                require('../images/PowerBank/P4/p44.jpg'),
+                            ],
+                            views: "6541",
+                            wishlist: "1987",
+                        },
+                        {
+                            key: 'gf8718vbs4',
+                            imgUrl: require('../images/PowerBank/P5/p51.jpg'),
+                            name: "Vooni® Stone",
+                            description: {
+                                features: [{
+                                    info: "Vooni® Stone Power Bank 0400mAh",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/PowerBank/P5/p52.jpg'),
+                                require('../images/PowerBank/P5/p53.jpg'),
+                                require('../images/PowerBank/P5/p54.jpg'),
+                            ],
+                            views: "3957",
+                            wishlist: "989",
+                        },
+
+
+                    ],
+                    
+            },
+
+
+            {
+                key: 'jvxf706zx1371jhd11',
+                imgUrl: require('../images/Chargeur/p.jpg'),
+                name: 'Chargeur Universel Téléphone',
+                    products: [
+                        {
+                            name: "CHARGEUR TÉLÉPHONE",
+                            description: {
+                                features: [{
+                                    info: "Chargeur secteur + câble data Samsung GALAXY S3 S4",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/Chargeur/P1/p11.jpg'),
+                                require('../images/Chargeur/P1/p12.jpg'),
+                                require('../images/Chargeur/P1/p13.jpg'),
+                                require('../images/Chargeur/P1/p14.jpg'),
+                            ],
+                            views: "2765",
+                            wishlist: "628",
+                        },
+                        {
+                            key: 'jvxf706zx1371jhd11',
+                            imgUrl: require('../images/Chargeur/P2/p21.jpg'),
+                            name: "Chargeur PHILIPS",
+                            description: {
+                                features: [{
+                                    info: "Utilisez le chargeur compact et pratique avec fiche repliable pour recharger votre iPhone ou votre iPod depuis n’importe quelle prise murale. Il offre plus de puissance pour une charge complète encore plus rapide.",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/Chargeur/P2/p22.jpg'),
+                                require('../images/Chargeur/P2/p23.jpg'),
+                                require('../images/Chargeur/P2/p24.jpg'),
+                            ],
+                            views: "6729",
+                            wishlist: "423",
+                        },
+                        {
+                            key: 'jjhdh567j2',
+                            imgUrl: require('../images/Chargeur/P3/p31.jpg'),
+                            name: "Chargeur sans fil",
+                            description: {
+                                features: [{
+                                    info: "Nexus 4 : le chargeur sans fil à induction fait son apparition",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/Chargeur/P3/p32.jpg'),
+                                require('../images/Chargeur/P3/p33.jpg'),
+                                require('../images/Chargeur/P3/p34.jpg'),
+                                require('../images/Chargeur/P3/p35.jpg'),
+                            ],
+                            views: "6758",
+                            wishlist: "333",
+                        },
+                        {
+                            key: 'fghdh567g43',
+                            imgUrl: require('../images/Chargeur/P4/p41.jpg'),
+                            name: "Chargeur Bluetooth",
+                            description: {
+                                features: [{
+                                    info: "Avec Fuel, ne tombez jamais à court de batterie pour votre smartphone ",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/Chargeur/P4/p42.jpg'),
+                                require('../images/Chargeur/P4/p43.jpg'),
+                                require('../images/Chargeur/P4/p44.jpg'),
+                            ],
+                            views: "3148",
+                            wishlist: "897",
+                        },
+                        {
+                            key: 'jjhdh554hgf8',
+                            imgUrl: require('../images/Chargeur/P5/p51.jpg'),
+                            name: "Chargeur Multifonction",
+                            description: {
+                                features: [{
+                                    info: "Chargeur multifonction compatible avec les batteries GOPRO",
+                                },]
+                                
+                            },
+                            productsImg: [
+                                require('../images/Chargeur/P5/p52.jpg'),
+                                require('../images/Chargeur/P5/p53.jpg'),
+                                require('../images/Chargeur/P5/p54.jpg'),
+                                require('../images/Chargeur/P5/p55.jpg'),
+                            ],
+                            views: "4445",
+                            wishlist: "765",
+                        },
+
+
+                    ],
+            },
+
+
+            {
+                key: 'ghhdt5676JG',
+                imgUrl: require(''),
+                name: 'Accessoires téléphoniques',
+                products: [
+                    {
+                        key: 'Gfdsr43ik',
+                        imgUrl: require('../images/Accessoires/P1/p16.jpg'),
+                        name: "Coq de Téléphone",
+                        description: {
+                            features: [{
+                                info: "Les meilleures Coq de téléphone",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P1/p12.jpg'),
+                            require('../images/Accessoires/P1/p13.jpg'),
+                            require('../images/Accessoires/P1/p14.jpg'),
+                            require('../images/Accessoires/P1/p15.jpg'),
+                        ],
+                        views: "4887",
+                        wishlist: "466",
+                    },
+                    {
+                        key: 'Gfdsr43jZ7',
+                        imgUrl: require('../images/Accessoires/P2/p21.jpg'),
+                        name: "Écouteurs",
+                        description: {
+                            features: [{
+                                info: "Casque Ecouteur Moto Helmet Earphone Avec Micro Haut-Parleur Pour MP3 Téléphone",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P2/p22.jpg'),
+                            require('../images/Accessoires/P2/p23.jpg'),
+                            require('../images/Accessoires/P2/p24.jpg'),
+                            require('../images/Accessoires/P2/p25.jpg'),
+                        ],
+                        views: "5000",
+                        wishlist: "544",
+                    },
+                    {
+                        key: 'Gfdsr43Df45',
+                        imgUrl: require('../images/Accessoires/P3/p31.jpg'),
+                        name: "Micro Casque",
+                        description: {
+                            features: [{
+                                info: "Mpow Micro Casque PC Audio Filaire Casque Téléphone Ecouteur,Casque USB",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P3/p32.jpg'),
+                            require('../images/Accessoires/P3/p33.jpg'),
+                            require('../images/Accessoires/P3/p34.jpg'),
+                            require('../images/Accessoires/P3/p35.jpg'),
+                        ],
+                        views: "2119",
+                        wishlist: "771",
+                    },
+                    {
+                        key: 'Gfds19hGG77',
+                        imgUrl: require('../images/Accessoires/P4/p41.jpg'),
+                        name: "Accessoires pour Mobiles",
+                        description: {
+                            features: [{
+                                info: "Pack d'accessoires téléphones, Coques, Housses",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P4/p42.jpg'),
+                            require('../images/Accessoires/P4/p4.3jpg'),
+                            require('../images/Accessoires/P4/p44.jpg'),
+                            require('../images/Accessoires/P4/p45.jpg'),
+                        ],
+                        views: "5533",
+                        wishlist: "4532",
+                    },
+                    {
+                        key: 'Gfds19hvFF23',
+                        imgUrl: require('../images/Accessoires/P5/p51.jpg'),
+                        name: "Étui Téléphone",
+                        description: {
+                            features: [{
+                                info: "Étui de Téléphone Pochette Wiko Buk Lenny",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P5/p52.jpg'),
+                            require('../images/Accessoires/P5/p53.jpg'),
+                            require('../images/Accessoires/P5/p54.jpg'),
+                            require('../images/Accessoires/P5/p55.jpg'),
+                            require('../images/Accessoires/P5/p56.jpg'),
+                        ],
+                        views: "3434",
+                        wishlist: "555",
+                    },
+                    {
+                        key: '',
+                        imgUrl: require('../images/Accessoires/P6/cas1.jpg'),
+                        name: "Anti-Casse",
+                        description: {
+                            features: [{
+                                info: "Verre trempé Samsung Galaxy 3 Film protection écran en VERRE Trempé - Vitre protecteur anti casse, anti-rayure ",
+                                dimensions: "134.5 x 65.2 x 7.3 mm, écran 4.7",
+                                pose: "Très facile à appliquer, pose sans bulles, anti poussières",
+                            },]
+                            
+                        },
+                        productsImg: [
+                            require('../images/Accessoires/P6/cas2.jpg'),
+                            require('../images/Accessoires/P6/cas3.jpg'),
+                            require('../images/Accessoires/P6/cas4.jpg'),
+                            require('../images/Accessoires/P6/cas5.jpg'),
+                            require('../images/Accessoires/P6/cas6.jpg'),
+                        ],
+                        views: "6654",
+                        wishlist: "670",
+                    },
+                ],
+            },
+
+
+
+            {
+                key: '12er2r',
+                imgUrl: require('../images/pictures/phone.png'),
+                name: 'Pièces Telephone Mobile',
+            },
+
+
+            {
+                key: 'r223r42tr31r',
+                imgUrl: require('../images/pictures/phone.png'),
+                name: 'Pièces Telephone Mobile',
+            }
+        ]
+    },
 ]
