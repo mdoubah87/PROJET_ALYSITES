0187e283c5e43c9a229356165d12463ce1f94b64
mdoubah87 <mdoubah1410@gmail.com>
2018-10-12 17:29:30 +0000

    update accessoires

--- CATEGORIES/fait/Telephone_Portable_et_Accessoires/data/Telephone_Portable_et_Accessoires.js
+++ CATEGORIES/fait/Telephone_Portable_et_Accessoires/data/Telephone_Portable_et_Accessoires.js
@@ -248,7 +248,7 @@
             name: 'Accessoires téléphoniques',
             products: [{
                     key: 'Gfdsr43ik',
-                    imgUrl: require('../Images/Accessoires/P1/p16.jpg'),
+                    imgUrl: require('../Images/Accessoires/P1/p11.png'),
                     name: "Coque de Téléphone",
                     description: {
                         features: [{
@@ -257,17 +257,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P1/p12.jpg'),
-                        require('../Images/Accessoires/P1/p13.jpg'),
-                        require('../Images/Accessoires/P1/p14.jpg'),
-                        require('../Images/Accessoires/P1/p15.jpg'),
+                        require('../Images/Accessoires/P1/p12.png'),
+                        require('../Images/Accessoires/P1/p13.png'),
+                        require('../Images/Accessoires/P1/p14.png'),
+                        require('../Images/Accessoires/P1/p15.png'),
                     ],
                     views: "4887",
                     wishlist: "466",
                 },
                 {
                     key: 'Gfdsr43jZ7',
-                    imgUrl: require('../Images/Accessoires/P2/p21.jpg'),
+                    imgUrl: require('../Images/Accessoires/P2/p21.png'),
                     name: "Écouteurs",
                     description: {
                         features: [{
@@ -276,17 +276,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P2/p22.jpg'),
-                        require('../Images/Accessoires/P2/p23.jpg'),
-                        require('../Images/Accessoires/P2/p24.jpg'),
-                        require('../Images/Accessoires/P2/p25.jpg'),
+                        require('../Images/Accessoires/P2/p22.png'),
+                        require('../Images/Accessoires/P2/p23.png'),
+                        require('../Images/Accessoires/P2/p24.png'),
+                        require('../Images/Accessoires/P2/p25.png'),
                     ],
                     views: "5000",
                     wishlist: "544",
                 },
                 {
                     key: 'Gfdsr43Df45',
-                    imgUrl: require('../Images/Accessoires/P3/p31.jpg'),
+                    imgUrl: require('../Images/Accessoires/P3/p31.png'),
                     name: "Micro Casque",
                     description: {
                         features: [{
@@ -295,17 +295,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P3/p32.jpg'),
-                        require('../Images/Accessoires/P3/p33.jpg'),
-                        require('../Images/Accessoires/P3/p34.jpg'),
-                        require('../Images/Accessoires/P3/p35.jpg'),
+                        require('../Images/Accessoires/P3/p32.png'),
+                        require('../Images/Accessoires/P3/p33.png'),
+                        require('../Images/Accessoires/P3/p34.png'),
+                        require('../Images/Accessoires/P3/p35.png'),
                     ],
                     views: "2119",
                     wishlist: "771",
                 },
                 {
                     key: 'Gfds19hGG77',
-                    imgUrl: require('../Images/Accessoires/P4/p41.jpg'),
+                    imgUrl: require('../Images/Accessoires/P4/p41.png'),
                     name: "Accessoires pour Mobiles",
                     description: {
                         features: [{
@@ -314,17 +314,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P4/p42.jpg'),
-                        require('../Images/Accessoires/P4/p4.3jpg'),
-                        require('../Images/Accessoires/P4/p44.jpg'),
-                        require('../Images/Accessoires/P4/p45.jpg'),
+                        require('../Images/Accessoires/P4/p42.png'),
+                        require('../Images/Accessoires/P4/p43.png'),
+                        require('../Images/Accessoires/P4/p44.png'),
                     ],
                     views: "5533",
                     wishlist: "4532",
                 },
                 {
                     key: 'Gfds19hvFF23',
-                    imgUrl: require('../Images/Accessoires/P5/p51.jpg'),
+                    imgUrl: require('../Images/Accessoires/P5/p51.png'),
                     name: "Étui Téléphone",
                     description: {
                         features: [{
@@ -333,18 +332,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P5/p52.jpg'),
-                        require('../Images/Accessoires/P5/p53.jpg'),
-                        require('../Images/Accessoires/P5/p54.jpg'),
-                        require('../Images/Accessoires/P5/p55.jpg'),
-                        require('../Images/Accessoires/P5/p56.jpg'),
+                        require('../Images/Accessoires/P5/p52.png'),
+                        require('../Images/Accessoires/P5/p53.png'),
+                        require('../Images/Accessoires/P5/p54.png'),
+                        require('../Images/Accessoires/P5/p55.png'),
                     ],
                     views: "3434",
                     wishlist: "555",
                 },
                 {
                     key: '',
-                    imgUrl: require('../Images/Accessoires/P6/cas1.jpg'),
+                    imgUrl: require('../Images/Accessoires/P6/p61.png'),
                     name: "Anti-Casse",
                     description: {
                         features: [{
@@ -355,11 +353,10 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P6/cas2.jpg'),
-                        require('../Images/Accessoires/P6/cas3.jpg'),
-                        require('../Images/Accessoires/P6/cas4.jpg'),
-                        require('../Images/Accessoires/P6/cas5.jpg'),
-                        require('../Images/Accessoires/P6/cas6.jpg'),
+                        require('../Images/Accessoires/P6/p62.png'),
+                        require('../Images/Accessoires/P6/p63.png'),
+                        require('../Images/Accessoires/P6/p64.png'),
+                        require('../Images/Accessoires/P6/p65.png'),
                     ],
                     views: "6654",
                     wishlist: "670",
