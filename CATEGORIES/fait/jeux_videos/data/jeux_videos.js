da80a7760c025979d5935b7e38b4d12e1f879996
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-13 12:39:25 +0000

    ajout de la categorie 'jeu videos '

---
+++ CATEGORIES/fait/jeux_videos/data/jeux_videos.js
@@ -0,0 +1,137 @@
+[
+
+    {
+        key: 'fgkkf84jkdfv57f3d5dfifi4jur857',
+        imgUrl: require('../images/1.png'),
+        name: 'Jeu Videos',
+        subCategories: 
+        [
+            
+            {
+                key: '12ejh5445fghfrrfdfgefe120gg2d3r',
+                imgUrl: require('../images/pictures/j_ps3/1.png'),
+                name: 'Jeu PlayStation 3',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: '12ejh45fghffdfgefe12yjyt0gg2d3r',
+                imgUrl: require('../images/pictures/accesoires/1.png'),
+                name: 'Accessoires de jeu',
+                products: 
+                [
+                   
+                ]
+            },
+            {
+                key: 'yyyjnvh676dds4sd76fghjhhhjhjh',
+                imgUrl: require('../images/pictures/j_ps4/1.png'),
+                name: 'Jeu PlayStation 4',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu6sfrrtrrrr578ts5e3',
+                imgUrl: require('../images/pictures/j_switch/1.png'),
+                name: 'Jeu Nintendo Switch',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rsfdf43rre3',
+                imgUrl: require('../images/pictures/j_xbox_360/1.png'),
+                name: 'Jeu Xbox 360',
+                products: 
+                [
+                   
+                ]
+            },
+            {
+                key: 'fyuu65785dd4ts5rrre3',
+                imgUrl: require('../images/pictures/j_xbox_one/1.png'),
+                name: 'jeu Xbox one ',
+                products: 
+                [
+                   
+                ]
+            },
+            {
+                key: 'fyuu657de8fd54fg5dts5rrre3',
+                imgUrl: require('../images/pictures/nitendo_switch/1.png'),
+                name: 'Console Nitendo Switch ',
+                products: 
+                [
+                   
+                ]
+            },
+            {
+                key: 'fyuu6578rd5d5drts5rrre3',
+                imgUrl: require('../images/pictures/nitendo_ds/1.png'),
+                name: 'Console Nitendo DS',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rfsmhj498rre3',
+                imgUrl: require('../images/pictures/pc/1.png'),
+                name: 'Jeu pour PC',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'fyuu6ffd4f578ts5rrre3',
+                imgUrl: require('../images/pictures/ps3/1.png'),
+                name: 'Console PlayStation 3',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu657frgrt544tts5rrre3',
+                imgUrl: require('../images/pictures/ps4/1.png'),
+                name: 'Console PlayStation 3',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: 'fyuu657frgrt544tts5rrre3',
+                imgUrl: require('../images/pictures/xbox_360/1.png'),
+                name: 'Console Xbox 360 ',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'fyuu657frgrt544ttff454s5rrre3',
+                imgUrl: require('../images/pictures/xbox_one/1.png'),
+                name: 'Console Xbox ONE ',
+                products: 
+                [
+                    
+                ]
+            },
+        ],
+    },
+
+ 
+]
