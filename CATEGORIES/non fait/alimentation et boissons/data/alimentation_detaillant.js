0c02596f3b32e3aa7ab6544b2336bbcc75cd54ba
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-15 17:23:59 +0000

    ajout de la categorie 'alimentation' mais pas ajouté dans Fait

---
+++ CATEGORIES/non fait/alimentation et boissons/data/alimentation_detaillant.js
@@ -0,0 +1,62 @@
+[
+
+    {
+        key: 'fgkkf84jkdfv57ffifi4jur857',
+        imgUrl: require('../images/1.png'),
+        name: 'Alimentation',
+        subCategories: 
+        [
+            
+            {
+                key: '12ejh5445fghffdfgefe120gg2d3r',
+                imgUrl: require('../images/pictures/bebe/1.png'),
+                name: 'Nutrition pour Bébé',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: '12ejh45fghffdfgefe12yjyt0gg2d3r',
+                imgUrl: require('../images/pictures/boissons/1.png'),
+                name: 'Boisson',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'yyyjnvh67676fghjhhhjhjh',
+                imgUrl: require('../images/pictures/huile_vegetal_animal/1.png'),
+                name: 'Huile vegetal et animal',
+                products: 
+                [
+                    
+                ]
+            },
+            {
+                key: 'fyuu6578ts5e3',
+                imgUrl: require('../images/pictures/poisson_fruit/1.png'),
+                name: 'Poisson et Fruit de mer',
+                products: 
+                [
+                   
+                ]
+            },
+            {
+                key: 'fyuu6578ts5rrre3',
+                imgUrl: require('../images/pictures/produit_origine_animal/1.png'),
+                name: "Produit d'origine Animal",
+                products: 
+                [
+                   
+                ]
+            },
+           
+            
+            
+        ],
+    },
+
+ 
+]
