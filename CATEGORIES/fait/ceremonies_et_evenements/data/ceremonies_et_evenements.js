fb0d8e04a7a01b43d2a019ce5c86d8954afa5bf2
mdoubah87 <mdoubah1410@gmail.com>
2018-10-16 10:35:52 +0000

    update ceremonie et evenement

---
+++ CATEGORIES/fait/ceremonies_et_evenements/data/ceremonies_et_evenements.js
@@ -0,0 +1,70 @@
+[
+
+    {
+        key: '',
+        imgUrl: require('../images/1.png'),
+        name: 'Cérémonie et Évènement',
+        subCategories: 
+        [
+            
+            {
+                key: '',
+                imgUrl: require('../images/Accessoires_de_Mariage/1.png'),
+                name: 'Accessoires de Mariages',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Decoration/1.png'),
+                name: 'DÉCORATION',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Installation_chaine_musicale/1.png'),
+                name: 'Installation enceinte musical',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Robe_de_Mariee/1.png'),
+                name: 'ROBE DE MARIÉE',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Robe_de_Soiree/1.png'),
+                name: 'ROBE DE SOIRÉE',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            
+        ],
+    },
+
+ 
+]
