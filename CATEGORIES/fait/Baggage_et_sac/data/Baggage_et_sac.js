047cde4729795ae8f87d1cdd12872c0206f35a79
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-16 16:52:43 +0000

    retification

--- CATEGORIES/fait/Baggage_et_sac/data/Baggage_et_sac.js
+++ CATEGORIES/fait/Baggage_et_sac/data/Baggage_et_sac.js
@@ -1,14 +1,14 @@
 [
 
     {
-        key: 'fgkkf84jkdfv57ffifi4jf6ff6ur857',
+        key: '',
         imgUrl: require('../images/1.png'),
         name: 'Bagage et Sac',
         subCategories: 
         [
             
             {
-                key: '12ejh5445fghfff5ffdfgefe120gg2d3r',
+                key: '',
                 imgUrl: require('../images/pictures/enfant/1.png'),
                 name: 'Sacs pour enfant',
                 products: 
@@ -17,7 +17,7 @@
                 ]
             },
             {
-                key: '12ejh45fghffffdsdfgefe12yjyt0gg2d3r',
+                key: '',
                 imgUrl: require('../images/pictures/porte_feuill/1.png'),
                 name: 'Porte Feuille',
                 products: 
@@ -26,7 +26,7 @@
                 ]
             },
             {
-                key: 'yyyjnvhfdffdr67676fghjhhhjhjh',
+                key: '',
                 imgUrl: require('../images/pictures/sac_bandouliere/1.png'),
                 name: 'Sac Bandoulière',
                 products: 
@@ -35,7 +35,7 @@
                 ]
             },
             {
-                key: 'fyuu6578tdfdfdsdfsds5e3',
+                key: '',
                 imgUrl: require('../images/pictures/sacs_banane/1.png'),
                 name: 'Sacs Banane',
                 products: 
@@ -44,7 +44,7 @@
                 ]
             },
             {
-                key: 'fyuu655f45f78ts5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/Sacs_docu_ordi/1.png'),
                 name: 'Porte Document et sacs Ordinateur',
                 products: 
@@ -53,7 +53,7 @@
                 ]
             },
             {
-                key: 'fyuu65784tddkljhfds5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/sacs_dos/1.png'),
                 name: 'Sacs à Dos',
                 products: 
@@ -62,7 +62,7 @@
                 ]
             },
             {
-                key: 'fyuu6578tddffddfds5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/sacs_mains/1.png'),
                 name: 'Sacs à mains',
                 products: 
@@ -73,7 +73,7 @@
                 ]
             },
             {
-                key: 'fyuu65dfdsaf78rrts5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/sacs_sport/1.png'),
                 name: 'Sacs de Sport',
                 products: 
@@ -82,7 +82,7 @@
                 ]
             },
             {
-                key: 'fyuu65dfdhffd78ts5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/sacs_voyage/1.png'),
                 name: 'Sacs de voyage',
                 products: 
@@ -91,7 +91,7 @@
                 ]
             },
             {
-                key: 'fyuu657dfhjhfjd8ts5rrre3',
+                key: '',
                 imgUrl: require('../images/pictures/valises/1.png'),
                 name: 'Valises',
                 products: 
