5ca9271061a889d80084c5f668c3a1146a184fa9
mdoubah87 <mdoubah1410@gmail.com>
2018-10-17 08:31:02 +0000

    update project alysites

--- CATEGORIES/fait/Telephone_Portable_et_Accessoires/data/Telephone_Portable_et_Accessoires.js
+++ CATEGORIES/fait/Telephone_Portable_et_Accessoires/data/Telephone_Portable_et_Accessoires.js
@@ -1,14 +1,14 @@
 [{
     key: 'jvcxf72zx1398jhf76',
-    imgUrl: require('../Images/phone.png'),
+    imgUrl: require('../images/phone.png'),
     name: 'Telephones portables et Accessoires',
     subCategories: [{
             key: 'jvcxf56zx1371jhf43',
-            imgUrl: require('../Images/PowerBank/pw4.png'),
+            imgUrl: require('../images/PowerBank/pw4.png'),
             name: 'Power Bank',
             products: [{
                     key: 'gh5678vbg7',
-                    imgUrl: require('../Images/PowerBank/pw1.png'),
+                    imgUrl: require('../images/PowerBank/pw1.png'),
                     name: "Battery Charger Solar",
                     description: {
                         features: [{
@@ -17,17 +17,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P1/pw1.png'),
-                        require('../Images/PowerBank/P1/pw2.png'),
-                        require('../Images/PowerBank/P1/pw3.png'),
-                        require('../Images/PowerBank/P1/pw4.png'),
+                        require('../images/PowerBank/P1/pw1.png'),
+                        require('../images/PowerBank/P1/pw2.png'),
+                        require('../images/PowerBank/P1/pw3.png'),
+                        require('../images/PowerBank/P1/pw4.png'),
                     ],
                     views: "2453",
                     wishList: "456",
                 },
                 {
                     key: 'gf8718vbs4',
-                    imgUrl: require('../Images/PowerBank/P2/p21.png'),
+                    imgUrl: require('../images/PowerBank/P2/p21.png'),
                     name: "ADATA PT100 Power Bank",
                     description: {
                         features: [{
@@ -36,16 +36,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P2/p22.png'),
-                        require('../Images/PowerBank/P2/p23.png'),
-                        require('../Images/PowerBank/P2/p24.png'),
+                        require('../images/PowerBank/P2/p22.png'),
+                        require('../images/PowerBank/P2/p23.png'),
+                        require('../images/PowerBank/P2/p24.png'),
                     ],
                     views: "2451",
                     wishList: "456",
                 },
                 {
                     key: 'gf8718vbs4',
-                    imgUrl: require('../Images/PowerBank/P3/p31.png'),
+                    imgUrl: require('../images/PowerBank/P3/p31.png'),
                     name: "Xiaomi Mi",
                     description: {
                         features: [{
@@ -54,16 +54,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P3/p32.png'),
-                        require('../Images/PowerBank/P3/p33.png'),
-                        require('../Images/PowerBank/P3/p34.png'),
+                        require('../images/PowerBank/P3/p32.png'),
+                        require('../images/PowerBank/P3/p33.png'),
+                        require('../images/PowerBank/P3/p34.png'),
                     ],
                     views: "3214",
                     wishList: "239",
                 },
                 {
                     key: 'gf8718vbs4',
-                    imgUrl: require('../Images/PowerBank/P4/p41.jpg'),
+                    imgUrl: require('../images/PowerBank/P4/p41.jpg'),
                     name: "Digipower A26",
                     description: {
                         features: [{
@@ -72,16 +72,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P4/p42.jpg'),
-                        require('../Images/PowerBank/P4/p43.jpg'),
-                        require('../Images/PowerBank/P4/p44.jpg'),
+                        require('../images/PowerBank/P4/p42.jpg'),
+                        require('../images/PowerBank/P4/p43.jpg'),
+                        require('../images/PowerBank/P4/p44.jpg'),
                     ],
                     views: "6541",
                     wishList: "1987",
                 },
                 {
                     key: 'gf8718vbs4',
-                    imgUrl: require('../Images/PowerBank/P5/p51.png'),
+                    imgUrl: require('../images/PowerBank/P5/p51.png'),
                     name: "Vooni Stone",
                     description: {
                         features: [{
@@ -90,16 +90,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P5/p52.png'),
-                        require('../Images/PowerBank/P5/p53.png'),
-                        require('../Images/PowerBank/P5/p54.png'),
+                        require('../images/PowerBank/P5/p52.png'),
+                        require('../images/PowerBank/P5/p53.png'),
+                        require('../images/PowerBank/P5/p54.png'),
                     ],
                     views: "3957",
                     wishList: "989",
                 },
                 {
                     key: 'PwdFr4547hY67',
-                    imgUrl: require('../Images/PowerBank/P6/p61.png'),
+                    imgUrl: require('../images/PowerBank/P6/p61.png'),
                     name: "New iPower",
                     description: {
                         features: [{
@@ -108,9 +108,9 @@
 
                     },
                     productsImg: [
-                        require('../Images/PowerBank/P6/p62.png'),
-                        require('../Images/PowerBank/P6/p63.png'),
-                        require('../Images/PowerBank/P6/p64.png'),
+                        require('../images/PowerBank/P6/p62.png'),
+                        require('../images/PowerBank/P6/p63.png'),
+                        require('../images/PowerBank/P6/p64.png'),
                     ],
                     views: "5199",
                     wishList: "206",
@@ -124,7 +124,7 @@
 
         {
             key: 'jvxf706zx1371jhd11',
-            imgUrl: require('../Images/Chargeur/p.png'),
+            imgUrl: require('../images/Chargeur/p.png'),
             name: 'Chargeur Universel Téléphone',
             products: [{
                     name: "CHARGEUR TÉLÉPHONE",
@@ -135,17 +135,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P1/p11.png'),
-                        require('../Images/Chargeur/P1/p12.png'),
-                        require('../Images/Chargeur/P1/p13.png'),
-                        require('../Images/Chargeur/P1/p14.png'),
+                        require('../images/Chargeur/P1/p11.png'),
+                        require('../images/Chargeur/P1/p12.png'),
+                        require('../images/Chargeur/P1/p13.png'),
+                        require('../images/Chargeur/P1/p14.png'),
                     ],
                     views: "2765",
                     wishList: "628",
                 },
                 {
                     key: 'jvxf706zx1371jhd11',
-                    imgUrl: require('../Images/Chargeur/P2/p21.png'),
+                    imgUrl: require('../images/Chargeur/P2/p21.png'),
                     name: "Chargeur PHILIPS",
                     description: {
                         features: [{
@@ -154,16 +154,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P2/p22.png'),
-                        require('../Images/Chargeur/P2/p23.png'),
-                        require('../Images/Chargeur/P2/p24.png'),
+                        require('../images/Chargeur/P2/p22.png'),
+                        require('../images/Chargeur/P2/p23.png'),
+                        require('../images/Chargeur/P2/p24.png'),
                     ],
                     views: "6729",
                     wishList: "423",
                 },
                 {
                     key: 'jjhdh567j2',
-                    imgUrl: require('../Images/Chargeur/P3/p31.png'),
+                    imgUrl: require('../images/Chargeur/P3/p31.png'),
                     name: "Chargeur sans fil",
                     description: {
                         features: [{
@@ -172,17 +172,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P3/p32.png'),
-                        require('../Images/Chargeur/P3/p33.png'),
-                        require('../Images/Chargeur/P3/p34.png'),
-                        require('../Images/Chargeur/P3/p35.png'),
+                        require('../images/Chargeur/P3/p32.png'),
+                        require('../images/Chargeur/P3/p33.png'),
+                        require('../images/Chargeur/P3/p34.png'),
+                        require('../images/Chargeur/P3/p35.png'),
                     ],
                     views: "6758",
                     wishList: "333",
                 },
                 {
                     key: 'fghdh567g43',
-                    imgUrl: require('../Images/Chargeur/P4/p41.png'),
+                    imgUrl: require('../images/Chargeur/P4/p41.png'),
                     name: "Chargeur Bluetooth",
                     description: {
                         features: [{
@@ -191,16 +191,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P4/p42.png'),
-                        require('../Images/Chargeur/P4/p43.png'),
-                        require('../Images/Chargeur/P4/p44.png'),
+                        require('../images/Chargeur/P4/p42.png'),
+                        require('../images/Chargeur/P4/p43.png'),
+                        require('../images/Chargeur/P4/p44.png'),
                     ],
                     views: "3148",
                     wishList: "897",
                 },
                 {
                     key: 'jjhdh554hgf8',
-                    imgUrl: require('../Images/Chargeur/P5/p51.png'),
+                    imgUrl: require('../images/Chargeur/P5/p51.png'),
                     name: "Chargeur Multifonction",
                     description: {
                         features: [{
@@ -209,17 +209,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P5/p52.png'),
-                        require('../Images/Chargeur/P5/p53.png'),
-                        require('../Images/Chargeur/P5/p54.png'),
-                        require('../Images/Chargeur/P5/p55.png'),
+                        require('../images/Chargeur/P5/p52.png'),
+                        require('../images/Chargeur/P5/p53.png'),
+                        require('../images/Chargeur/P5/p54.png'),
+                        require('../images/Chargeur/P5/p55.png'),
                     ],
                     views: "4445",
                     wishList: "765",
                 },
                 {
                     key: 'jjhdhgTTy67',
-                    imgUrl: require('../Images/Chargeur/P6/p61.png'),
+                    imgUrl: require('../images/Chargeur/P6/p61.png'),
                     name: "Chargeur Murale USB",
                     description: {
                         features: [{
@@ -228,10 +228,10 @@
 
                     },
                     productsImg: [
-                        require('../Images/Chargeur/P6/p62.png'),
-                        require('../Images/Chargeur/P6/p63.png'),
-                        require('../Images/Chargeur/P6/p64.png'),
-                        require('../Images/Chargeur/P6/p65.png'),
+                        require('../images/Chargeur/P6/p62.png'),
+                        require('../images/Chargeur/P6/p63.png'),
+                        require('../images/Chargeur/P6/p64.png'),
+                        require('../images/Chargeur/P6/p65.png'),
                     ],
                     views: "6590",
                     wishList: "2345",
@@ -248,7 +248,7 @@
             name: 'Accessoires téléphoniques',
             products: [{
                     key: 'Gfdsr43ik',
-                    imgUrl: require('../Images/Accessoires/P1/p11.png'),
+                    imgUrl: require('../images/Accessoires/P1/p11.png'),
                     name: "Coque de Téléphone",
                     description: {
                         features: [{
@@ -257,17 +257,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P1/p12.png'),
-                        require('../Images/Accessoires/P1/p13.png'),
-                        require('../Images/Accessoires/P1/p14.png'),
-                        require('../Images/Accessoires/P1/p15.png'),
+                        require('../images/Accessoires/P1/p12.png'),
+                        require('../images/Accessoires/P1/p13.png'),
+                        require('../images/Accessoires/P1/p14.png'),
+                        require('../images/Accessoires/P1/p15.png'),
                     ],
                     views: "4887",
                     wishList: "466",
                 },
                 {
                     key: 'Gfdsr43jZ7',
-                    imgUrl: require('../Images/Accessoires/P2/p21.png'),
+                    imgUrl: require('../images/Accessoires/P2/p21.png'),
                     name: "Écouteurs",
                     description: {
                         features: [{
@@ -276,17 +276,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P2/p22.png'),
-                        require('../Images/Accessoires/P2/p23.png'),
-                        require('../Images/Accessoires/P2/p24.png'),
-                        require('../Images/Accessoires/P2/p25.png'),
+                        require('../images/Accessoires/P2/p22.png'),
+                        require('../images/Accessoires/P2/p23.png'),
+                        require('../images/Accessoires/P2/p24.png'),
+                        require('../images/Accessoires/P2/p25.png'),
                     ],
                     views: "5000",
                     wishList: "544",
                 },
                 {
                     key: 'Gfdsr43Df45',
-                    imgUrl: require('../Images/Accessoires/P3/p31.png'),
+                    imgUrl: require('../images/Accessoires/P3/p31.png'),
                     name: "Micro Casque",
                     description: {
                         features: [{
@@ -295,17 +295,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P3/p32.png'),
-                        require('../Images/Accessoires/P3/p33.png'),
-                        require('../Images/Accessoires/P3/p34.png'),
-                        require('../Images/Accessoires/P3/p35.png'),
+                        require('../images/Accessoires/P3/p32.png'),
+                        require('../images/Accessoires/P3/p33.png'),
+                        require('../images/Accessoires/P3/p34.png'),
+                        require('../images/Accessoires/P3/p35.png'),
                     ],
                     views: "2119",
                     wishList: "771",
                 },
                 {
                     key: 'Gfds19hGG77',
-                    imgUrl: require('../Images/Accessoires/P4/p41.png'),
+                    imgUrl: require('../images/Accessoires/P4/p41.png'),
                     name: "Accessoires pour Mobiles",
                     description: {
                         features: [{
@@ -314,16 +314,16 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P4/p42.png'),
-                        require('../Images/Accessoires/P4/p43.png'),
-                        require('../Images/Accessoires/P4/p44.png'),
+                        require('../images/Accessoires/P4/p42.png'),
+                        require('../images/Accessoires/P4/p43.png'),
+                        require('../images/Accessoires/P4/p44.png'),
                     ],
                     views: "5533",
                     wishList: "4532",
                 },
                 {
                     key: 'Gfds19hvFF23',
-                    imgUrl: require('../Images/Accessoires/P5/p51.png'),
+                    imgUrl: require('../images/Accessoires/P5/p51.png'),
                     name: "Étui Téléphone",
                     description: {
                         features: [{
@@ -332,17 +332,17 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P5/p52.png'),
-                        require('../Images/Accessoires/P5/p53.png'),
-                        require('../Images/Accessoires/P5/p54.png'),
-                        require('../Images/Accessoires/P5/p55.png'),
+                        require('../images/Accessoires/P5/p52.png'),
+                        require('../images/Accessoires/P5/p53.png'),
+                        require('../images/Accessoires/P5/p54.png'),
+                        require('../images/Accessoires/P5/p55.png'),
                     ],
                     views: "3434",
                     wishList: "555",
                 },
                 {
                     key: '',
-                    imgUrl: require('../Images/Accessoires/P6/p61.png'),
+                    imgUrl: require('../images/Accessoires/P6/p61.png'),
                     name: "Anti-Casse",
                     description: {
                         features: [{
@@ -353,10 +353,10 @@
 
                     },
                     productsImg: [
-                        require('../Images/Accessoires/P6/p62.png'),
-                        require('../Images/Accessoires/P6/p63.png'),
-                        require('../Images/Accessoires/P6/p64.png'),
-                        require('../Images/Accessoires/P6/p65.png'),
+                        require('../images/Accessoires/P6/p62.png'),
+                        require('../images/Accessoires/P6/p63.png'),
+                        require('../images/Accessoires/P6/p64.png'),
+                        require('../images/Accessoires/P6/p65.png'),
                     ],
                     views: "6654",
                     wishList: "670",
