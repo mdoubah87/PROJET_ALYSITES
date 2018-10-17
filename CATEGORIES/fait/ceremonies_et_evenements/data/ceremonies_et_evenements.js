468fb61b2b1dc22240c542f4a6f0feb761305164
mdoubah87 <mdoubah1410@gmail.com>
2018-10-17 12:08:49 +0000

    Ajout du doc Mariage et ses produits

--- CATEGORIES/fait/ceremonies_et_evenements/data/ceremonies_et_evenements.js
+++ CATEGORIES/fait/ceremonies_et_evenements/data/ceremonies_et_evenements.js
@@ -31,6 +31,39 @@
             },
             {
                 key: '',
+                imgUrl: require('../images/Emballages/1.png'),
+                name: 'EMBALLAGES',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Accessoires_de_fetes/1.png'),
+                name: 'Accessoires de Fêtes',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/Vaisselle_jetable_et_plastique/1.png'),
+                name: 'Vaisselle Jetable et Plastiques',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
                 imgUrl: require('../images/Installation_chaine_musicale/1.png'),
                 name: 'Installation enceinte musical',
                 products: 
@@ -40,28 +73,98 @@
                     },
                 ]
             },
+
             {
                 key: '',
-                imgUrl: require('../images/Robe_de_Mariee/1.png'),
-                name: 'ROBE DE MARIÉE',
+                imgUrl: require('../images/Mariage/1.png'),
+                name: 'Mariage',
                 products: 
                 [
                     {
-                        
+                        name: "ROBES DE MARIÉES",
+                        description: {
+                            features: [{
+                                info: "Robes de mariées princesses ",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/Mariage/P1/p11.png'),
+                            require('../images/Mariage/P1/p12.png'),
+                            require('../images/Mariage/P1/p13.png'),
+                            require('../images/Mariage/P1/p14.png'),
+                            require('../images/Mariage/P1/p15.png'),
+                            require('../images/Mariage/P1/p16.png'),
+                            require('../images/Mariage/P1/p17.png'),
+                        ],
+                        views: "5665",
+                        wishList: "77",
                     },
-                ]
-            },
-            {
-                key: '',
-                imgUrl: require('../images/Robe_de_Soiree/1.png'),
-                name: 'ROBE DE SOIRÉE',
-                products: 
-                [
                     {
-                        
+                        name: "ROBES DE SOIRÉE",
+                        description: {
+                            features: [{
+                                info: "2018 Bourgogne robes de soirée velours longue sirène robes de bal hors épaule à manches longues",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/Mariage/P2/p21.png'),
+                            require('../images/Mariage/P2/p22.png'),
+                            require('../images/Mariage/P2/p23.png'),
+                            require('../images/Mariage/P2/p24.png'),
+                            require('../images/Mariage/P2/p25.png'),
+                            require('../images/Mariage/P2/p26.png'),
+                            require('../images/Mariage/P2/p27.png'),
+                            require('../images/Mariage/P2/p28.png'),
+                        ],
+                        views: "9112",
+                        wishList: "133",
+                    },
+                    {
+                        name: "BAGUES",
+                        description: {
+                            features: [{
+                                info: "AnazoZ Alliance Homme Femme Acier Inoxydable Zirconium 6MM Plaqué Or Bague de Mariage pour Deux Femme",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/Mariage/P3/p31.png'),
+                            require('../images/Mariage/P3/p32.png'),
+                            require('../images/Mariage/P3/p33.png'),
+                            require('../images/Mariage/P3/p34.png'),
+                            require('../images/Mariage/P3/p35.png'),
+                            require('../images/Mariage/P3/p36.png'),
+                            require('../images/Mariage/P3/p37.png'),
+                        ],
+                        views: "6554",
+                        wishList: "444",
+                    },
+                    {
+                        name: "BOUQUET DE FLEURS",
+                        description: {
+                            features: [{
+                                info: "Bouquet de fleurs tenant mariage ",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/Mariage/P4/p41.png'),
+                            require('../images/Mariage/P4/p42.png'),
+                            require('../images/Mariage/P4/p43.png'),
+                            require('../images/Mariage/P4/p44.png'),
+                        ],
+                        views: "6554",
+                        wishList: "444",
                     },
                 ]
             },
+            
             
         ],
     },
