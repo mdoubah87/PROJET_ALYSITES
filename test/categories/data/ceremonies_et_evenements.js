1b40dc9101bbe331b32db05e03b390fc374e7270
tidianeb5 <amadoutidianeb1@gmail.com>
2018-10-17 12:20:25 +0000

    ajout de la categorie ceromonies et evenements dans le dossier test

---
+++ test/categories/data/ceremonies_et_evenements.js
@@ -0,0 +1,173 @@
+[
+
+    {
+        key: '',
+        imgUrl: require('../images/ceremonies_et_evenements/1.png'),
+        name: 'Cérémonie et Évènement',
+        subCategories: 
+        [
+            
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Accessoires_de_Mariage/1.png'),
+                name: 'Accessoires de Mariages',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Decoration/1.png'),
+                name: 'DÉCORATION',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Emballages/1.png'),
+                name: 'EMBALLAGES',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Accessoires_de_fetes/1.png'),
+                name: 'Accessoires de Fêtes',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Vaisselle_jetable_et_plastique/1.png'),
+                name: 'Vaisselle Jetable et Plastiques',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Installation_chaine_musicale/1.png'),
+                name: 'Installation enceinte musical',
+                products: 
+                [
+                    {
+                        
+                    },
+                ]
+            },
+
+            {
+                key: '',
+                imgUrl: require('../images/ceremonies_et_evenements/Mariage/1.png'),
+                name: 'Mariage',
+                products: 
+                [
+                    {
+                        name: "ROBES DE MARIÉES",
+                        description: {
+                            features: [{
+                                info: "Robes de mariées princesses ",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p11.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p12.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p13.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p14.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p15.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p16.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P1/p17.png'),
+                        ],
+                        views: "5665",
+                        wishList: "77",
+                    },
+                    {
+                        name: "ROBES DE SOIRÉE",
+                        description: {
+                            features: [{
+                                info: "2018 Bourgogne robes de soirée velours longue sirène robes de bal hors épaule à manches longues",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p21.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p22.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p23.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p24.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p25.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p26.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p27.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P2/p28.png'),
+                        ],
+                        views: "9112",
+                        wishList: "133",
+                    },
+                    {
+                        name: "BAGUES",
+                        description: {
+                            features: [{
+                                info: "AnazoZ Alliance Homme Femme Acier Inoxydable Zirconium 6MM Plaqué Or Bague de Mariage pour Deux Femme",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p31.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p32.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p33.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p34.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p35.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p36.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P3/p37.png'),
+                        ],
+                        views: "6554",
+                        wishList: "444",
+                    },
+                    {
+                        name: "BOUQUET DE FLEURS",
+                        description: {
+                            features: [{
+                                info: "Bouquet de fleurs tenant mariage ",
+                            }, ]
+    
+                        },
+                        productsImg: 
+                        [
+                            require('../images/ceremonies_et_evenements/Mariage/P4/p41.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P4/p42.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P4/p43.png'),
+                            require('../images/ceremonies_et_evenements/Mariage/P4/p44.png'),
+                        ],
+                        views: "6554",
+                        wishList: "444",
+                    },
+                ]
+            },
+            
+            
+        ],
+    },
+
+ 
+]
